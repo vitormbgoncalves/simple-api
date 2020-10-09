d5b7ea7623f22ea1540b346255c84f23129c7581
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-09 18:34:35 -0300

    Category

---
+++ src/main/java/com/innovativetecnologia/simpleapi/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.innovativetecnologia.simpleapi.services;
+
+import com.innovativetecnologia.simpleapi.entities.Category;
+import com.innovativetecnologia.simpleapi.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
