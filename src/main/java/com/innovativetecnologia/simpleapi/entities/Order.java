0068846fc9d22ef9ec7188e399a6cf0924de0a35
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-09 16:46:04 -0300

    OrderStatus enum

--- src/main/java/com/innovativetecnologia/simpleapi/entities/Order.java
+++ src/main/java/com/innovativetecnologia/simpleapi/entities/Order.java
@@ -1,6 +1,7 @@
 package com.innovativetecnologia.simpleapi.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.innovativetecnologia.simpleapi.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,6 +20,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -26,9 +29,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +50,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getclient() {
