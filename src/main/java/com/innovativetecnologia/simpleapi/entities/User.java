708ebd96913b7ccc1ddce2e59737ad51d23c1905
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-09 11:38:31 -0300

    H2 database, test profile, JPA

--- src/main/java/com/innovativetecnologia/simpleapi/entities/User.java
+++ src/main/java/com/innovativetecnologia/simpleapi/entities/User.java
@@ -1,11 +1,18 @@
 package com.innovativetecnologia.simpleapi.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
 
     private static final long serialVersionUID = -6475896122123696731L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
