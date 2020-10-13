e0de1eba88ec61240b711b856f5bcc9a14c3d5af
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-13 11:38:13 -0300

    User delete

--- src/main/java/com/innovativetecnologia/simpleapi/resources/UserResource.java
+++ src/main/java/com/innovativetecnologia/simpleapi/resources/UserResource.java
@@ -4,15 +4,11 @@ import com.innovativetecnologia.simpleapi.entities.User;
 import com.innovativetecnologia.simpleapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
@@ -40,5 +36,11 @@ public class UserResource {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable  Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
