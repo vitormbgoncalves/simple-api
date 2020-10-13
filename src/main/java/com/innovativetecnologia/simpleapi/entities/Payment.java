df3c69c3b30e2c96aa87633875dfa21f81bc4aba
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-13 09:40:00 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/innovativetecnologia/simpleapi/entities/Payment.java
@@ -0,0 +1,74 @@
+package com.innovativetecnologia.simpleapi.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = -1724005340730792996L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment)) return false;
+
+        Payment payment = (Payment) o;
+
+        if (id != null ? !id.equals(payment.id) : payment.id != null) return false;
+        if (moment != null ? !moment.equals(payment.moment) : payment.moment != null) return false;
+        return order != null ? order.equals(payment.order) : payment.order == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (moment != null ? moment.hashCode() : 0);
+        result = 31 * result + (order != null ? order.hashCode() : 0);
+        return result;
+    }
+}
